97f7e91890e3c6edaaec8eda9b37a98b2e68d278
chae9214 <chae9214@gmail.com>
2016-02-27 18:43:06 -0500

    Fix database.py imports

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Sequence
 from sqlalchemy.sql import func
 from sqlalchemy.orm import sessionmaker
 
@@ -11,8 +11,8 @@ Base = declarative_base()
 class Entry(Base):
 	__tablename__ = 'entries'
 	id = Column(Integer, Sequence('entry_id_seq'), primary_key=True)
-	time_created = Column(DateTime(timezone=True), server_default=sql.func.now())
-	time_updated = Column(DateTime(timezone=True), onupdate=sql.func.now())
+	time_created = Column(DateTime(timezone=True), server_default=func.now())
+	time_updated = Column(DateTime(timezone=True), onupdate=func.now())
 	content = Column(String(256))
 
 Base.metadata.create_all(engine)
@@ -24,3 +24,8 @@ session = Session()
 new_entry = Entry(content='Buy milk')
 session.add(new_entry)
 session.commit()
+
+for e in session.query(Entry).filter(Entry.content == 'Buy milk'):
+	print e.content
+	print e.id
+	print e.time_created
