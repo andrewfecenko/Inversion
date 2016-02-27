5eaef4695a235d1c4e333b21fc9795ab697dda33
Abrar Hussain <abrar.a.hussain@gmail.com>
2016-02-27 13:21:45 -0500

    added basic front page gridlayout pattern

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from kivy.app import App
 from kivy.app import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.config import Config
+from kivy.core.text import LabelBase
 
-class Journal(Widget):
-    pass
+class Journal(GridLayout):
+	pass
 
 class JournalApp(App):
-    
-    def build(self):
-        journal = Journal()
-        return journal
- 
+
+	def build(self):
+		journal = Journal()
+		return journal
+
+
 if __name__ == "__main__":
+    Config.set('graphics', 'width', '600')
+    Config.set('graphics', 'height', '600')
+
+    LabelBase.register(name='Modern Pictograms', fn_regular = 'modernpics.ttf')
     JournalApp().run()
