0ee8324f57f91d0d3047bbb3a0676eecf807c630
elsieyoung <soupzai@163.com>
2016-03-02 16:05:35 -0500

    add add_entry page

--- add_entry.py
+++ add_entry.py
@@ -1,6 +1,14 @@
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
+from kivy.uix.popup import Popup
+from kivy.uix.button import Button
 from kivy.properties import BooleanProperty
+from kivy.properties import StringProperty
+from kivy.core.window import Window
+
+# from database import create_entry
+
+total_entry_num = 3
+
 
 class AddEntry(BoxLayout):
 
@@ -10,8 +18,46 @@ class AddEntry(BoxLayout):
         super(AddEntry, self).__init__(**kwargs)
         # TODO: create method to check if day's tasks have already been entered
         self.added_tasks = False
+        self.entry_list = []
+
+    def add_new(self):
+        global total_entry_num
+        total_entry_num += 1
+        new = Entry(name="task " + str(total_entry_num))
+        self.children[1].children[0].add_widget(new)
 
     def submit_tasks(self):
-        print self.ids.task_one_entry.task_text
-        print self.ids.task_two_entry.task_text
-        print self.ids.task_three_entry.task_text
+        for child in self.children[1].children[0].children:
+            if child.task_text != '':
+                self.entry_list.append(child.task_text)
+            else:
+                content = Button(text='OK', pos_hint={'top': 1}, size_hint=(0.4, 0.25),
+                                 background_color=(52, 152, 219, 1), color=(0, 0, 0, 1))
+                popup = Popup(title='Please Fill All Required Field', title_size='20sp', content=content,
+                                auto_dismiss=False, size_hint=(0.4, 0.4))
+                content.bind(on_press=popup.dismiss)
+                popup.open()
+                break
+
+        print ','.join(self.entry_list)
+
+        # create_entry(entry_list)
+
+
+class Entry(BoxLayout):
+    name = StringProperty('')
+
+    def remove(self):
+        if self.parent is not None:
+            ind = self.name.split(' ')[1]
+            print ind
+            for child in self.parent.children:
+                if child.name.split(' ')[1] > ind:
+                    child.name = 'task ' + str(int(child.name.split(' ')[1]) - 1)
+            self.parent.remove_widget(self)
+            global total_entry_num
+            total_entry_num -= 1
+
+
+
+
