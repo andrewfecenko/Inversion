732e82558c1d58564d95d69db640da4960ff59b0
chae9214 <chae9214@gmail.com>
2016-03-15 16:36:51 -0400

    Add import ej'

--- db_model.py
+++ db_model.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Sequence, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import backref
 import datetime
 
 Base = declarative_base()
