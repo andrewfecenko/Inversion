23825e335edf3a52f3ef3ca5dda4d5008519d9dc
chae9214 <chae9214@gmail.com>
2016-03-15 16:34:03 -0400

    Add delete cascade

--- db_model.py
+++ db_model.py
@@ -21,10 +21,14 @@ class Entry(Base):
     # use one-to-one for summary and plans, one-to-many for rest
     summary = relationship('Summary', uselist=False, back_populates='entries')
     plan = relationship('Plan', uselist=False, back_populates='entries')
-    tasks = relationship('Task', backref='entries', lazy='dynamic')
-    completed_tasks = relationship('CompletedTask', backref='entries', lazy='dynamic')
-    knowledges = relationship('Knowledge', backref='entries', lazy='dynamic')
-    failure_points = relationship('FailurePoint', backref='entries', lazy='dynamic')
+    tasks = relationship('Task', 
+    	backref=backref('entries', cascade="all, delete-orphan"), lazy='dynamic')
+    completed_tasks = relationship('CompletedTask',
+    	backref=backref('entries', cascade="all, delete-orphan"), lazy='dynamic')
+    knowledges = relationship('Knowledge',
+    	backref=backref('entries', cascade="all, delete-orphan"), lazy='dynamic')
+    failure_points = relationship('FailurePoint',
+    	backref=backref('entries', cascade="all, delete-orphan"), lazy='dynamic')
 
 
 class Summary(Base):
@@ -45,7 +49,7 @@ class Plan(Base):
 
 class Task(Base):
     __tablename__ = 'tasks'
-    id = Column(Integer, Sequence('entry_id_seq'), primary_key=True)
+    id = Column(Integer, Sequence('entry_id_seq'), primary_key=True, cascade="all, delete-orphan")
     entry_id = Column(Integer, ForeignKey(Entry.id))
     time_created = Column(DateTime, default=datetime.datetime.now())
     time_updated = Column(DateTime, default=datetime.datetime.now())
@@ -54,21 +58,21 @@ class Task(Base):
 
 class CompletedTask(Base):
     __tablename__ = 'completed_tasks'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, cascade="all, delete-orphan")
     entry_id = Column(Integer, ForeignKey(Entry.id))
     content = Column(String(256))
 
 
 class Knowledge(Base):
     __tablename__ = 'knowledges' #plural s to avoid confusion
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, cascade="all, delete-orphan")
     entry_id = Column(Integer, ForeignKey(Entry.id))
     content = Column(String(256))
 
 
 class FailurePoint(Base):
     __tablename__ = 'failure_points'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, cascade="all, delete-orphan")
     entry_id = Column(Integer, ForeignKey(Entry.id))
     content = Column(String(256))
 
