8b8656dd859ba91ffe213205b7319daf3ddae6d8
Abrar Hussain <abrar.a.hussain@gmail.com>
2016-03-24 16:26:28 -0400

    added basic yesterday's mistakes code

--- review_mistakes.py
+++ review_mistakes.py
@@ -1,4 +1,30 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from db_function import get_days_entry
+from db_function import get_entry_failure_points
+from datetime import datetime, timedelta
+
 
 class ReviewMistakes(BoxLayout):
-	pass
+
+    def __init__(self, **kwargs):
+        super(ReviewMistakes, self).__init__(**kwargs)
+        yesterday = datetime.today() - timedelta(days=1)
+        self.yesterdays_mistakes = get_days_entry(givenday=yesterday)
+        self.generate_label_widgets()
+        self.display_mistakes()
+
+    def generate_label_widgets(self):
+        if self.yesterdays_mistakes is None:
+            return
+
+        for label in self.yesterdays_mistakes:
+            curr_label = Label()
+            self.children[1].add_widget(curr_label)
+
+    def display_mistakes(self):
+        if self.yesterdays_mistakes is None:
+            return
+
+        for ind, mistake in enumerate(self.yesterdays_mistakes):
+            self.children[1].children[ind].text = mistake
