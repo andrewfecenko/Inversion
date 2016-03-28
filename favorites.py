afcad9b2e64c05f56585e9ea0268ded7499d29c1
elsieyoung <soupzai@163.com>
2016-03-27 22:35:03 -0400

    add page

--- favorites.py
+++ favorites.py
@@ -1,7 +1,35 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.text import LabelBase
+from kivy.properties import StringProperty
+
+KIVY_FONTS = [
+    {
+        "name": "RobotoCondensed",
+        "fn_regular": "data/fonts/RobotoCondensed-Light.ttf",
+        "fn_bold": "data/fonts/RobotoCondensed-Regular.ttf",
+        "fn_italic": "data/fonts/RobotoCondensed-LightItalic.ttf",
+        "fn_bolditalic": "data/fonts/RobotoCondensed-Italic.ttf"
+    }
+]
+
+for font in KIVY_FONTS:
+    LabelBase.register(**font)
+
 
 class Favorites(BoxLayout):
 
-
     def __init__(self, **kwargs):
         super(Favorites, self).__init__(**kwargs)
+        self.mistakes_list = {'2016-02-31': 'My mistake last week',
+                              '2016-01-31': 'My mistake last month',
+                              '2015-02-31': 'My mistake last year'}
+        self.mistake_grid.bind(minimum_height=self.mistake_grid.setter('height'))
+
+        for date, mistake in self.mistakes_list.iteritems():
+            new = Mistake(date=date, mistake=mistake)
+            self.children[1].children[0].add_widget(new)
+
+
+class Mistake(BoxLayout):
+    date = StringProperty('')
+    mistake = StringProperty('')
