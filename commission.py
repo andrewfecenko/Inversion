de38b980b4dd85011500deab471bb8c8cdc228d2
asakomatsuoka <mtok29@gmail.com>
2016-04-02 20:10:36 -0400

    main page

---
+++ commission.py
@@ -0,0 +1,7 @@
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Commission(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(Commission, self).__init__(**kwargs)
---
+++ omission.py
@@ -0,0 +1,7 @@
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Omission(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(Omission, self).__init__(**kwargs)
